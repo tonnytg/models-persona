41f70ade4b610eb38ad70b7ba3c8b928ac14b999
Antonio Thomacelli <tonnytg@gmail.com>
2024-08-21 00:06:56 -0300

    feat: initial commit

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-  "log"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
 )
 
 func main() {
-  log.Println("Start creating your Persona friend")
+	log.Println("Start creating your Persona friend")
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GOOGLE_API_KEY")))
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+  imgData, _ := os.ReadFile("photo.jpg")
+	resp, err := model.GenerateContent(
+		ctx,
+		genai.Text("What's in this photo?"),
+		genai.ImageData("jpeg", imgData))
+
+  if err != nil {
+    log.Println("Error:", err)
+    os.Exit(1)
+  }
+  log.Println("Resp:", resp)
 }
